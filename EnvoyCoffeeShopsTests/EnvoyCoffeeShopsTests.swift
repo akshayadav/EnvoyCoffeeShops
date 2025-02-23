6d4b540732e97cee65fe086fbf4e3e778c1f35de
Akshay <akshayadav@gmail.com>
2025-02-23 13:44:01 -0800

    init

---
+++ EnvoyCoffeeShopsTests/EnvoyCoffeeShopsTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnvoyCoffeeShopsTests.swift
+//  EnvoyCoffeeShopsTests
+//
+//  Created by Akshay Yadav on 2/23/25.
+//
+
+import Testing
+@testable import EnvoyCoffeeShops
+
+struct EnvoyCoffeeShopsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
